4bd76d2d0fe58af53416eb47c013a47b5ae6459a
YehorNechvola <ynechvola@eva.ua>
2025-08-28 20:15:59 +0300

    Init commit

---
+++ TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by Rush_user on 28.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
